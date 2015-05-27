d2b6ea50410f7cfa64b02b9c22f6ec183777a0cc
Josh Steiner <josh@vitriolix.com>
2015-05-26 19:40:08 -0700

    rename beta_paths/ to beta/

--- generate_content.py
+++ generate_content.py
@@ -375,7 +375,7 @@ do_dir()
 """
 
 print "generating content for beta paths"
-pack_dir = "beta_paths"
+pack_dir = "beta"
 yaml_parent_dir = os.getcwd() + "/yaml/org.storymaker.app/" + pack_dir
 for f in os.listdir(yaml_parent_dir):
     yaml_dir = "%s/%s" % (yaml_parent_dir, f)
