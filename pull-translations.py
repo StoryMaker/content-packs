707af12074c83186538efd929e80a302f76f9490
Josh Steiner <josh@vitriolix.com>
2016-01-28 11:40:22 -0800

    switch to pulling from fa_IR not fa

--- pull-translations.py
+++ pull-translations.py
@@ -3,4 +3,5 @@
 import os
 
 #os.system("tx pull -l ru,ar,bs_BA,sr,es,fa,de,mk,ko,fr,rw,vi --minimum-perc=80") # pulling out bs_BA until the scripts deal with renaming the files appropriately ... or does the app handle it?
-os.system("tx pull -l ru,ar,sr,es,fa,de,mk,ko,fr,rw,vi --minimum-perc=80")
+#os.system("tx pull -l ru,ar,sr,es,fa,de,mk,ko,fr,rw,vi --minimum-perc=80")
+os.system("tx pull -l ru,ar,sr,es,fa_IR,de,mk,ko,fr,rw,vi --minimum-perc=80")
